48a8a1d85752becdbe877e9a60fa743685d28948
Alex Flom <alexander.flom@gmail.com>
2022-11-11 11:23:19 -0700

    added filtering

--- registry/handlers/attributes.go
+++ registry/handlers/attributes.go
@@ -71,112 +71,205 @@ func (ah *attributesHandler) GetAttributes(w http.ResponseWriter, r *http.Reques
 			log.Printf("error reading db: %v", err)
 		}
 
-		// Deduplicate the database response
+		// Filter the query
+
+		filter := make(map[digest.Digest]map[string]map[string][]json.RawMessage)
+		allresultm := make(map[digest.Digest]map[string]string)
+
 		for _, result := range results {
 
-			//jsn, _ := json.Marshal(&result.AttribVal)
-			//attrib := make(map[string][][]byte)
-			//log.Printf("deduplicating result %v", i)
-			//attrib[result.AttribKey] = append(attrib[result.AttribKey], jsn)
-			attributes := make(map[string]string)
-			//schema[result.Schema] = attrib
-			resultm[result.Digest] = attributes
+			log.Printf("started filtering results")
+
+			pair, err := json.Marshal(result.AttribVal)
+			if err != nil {
+				log.Printf("Error while filtering: %v", err)
+			}
+			var jsonData map[string]interface{}
+			log.Println("unmarshalling annotations")
+			if err := json.Unmarshal([]byte(pair), &jsonData); err != nil {
+				log.Printf("Error while filtering: %v", err)
+			}
+			for key, value := range jsonData {
+				jsonValue, err := json.Marshal(value)
+				if err != nil {
+					log.Printf("Error while filtering: %v", err)
+				}
+				s := make(map[string]map[string][]json.RawMessage)
+				at := make(map[string][]json.RawMessage)
+				at[key] = append(at[key], jsonValue)
+				s[result.Schema] = at
+				filter[result.Digest] = s
+
+				attributes := make(map[string]string)
+				//schema[result.Schema] = attrib
+				allresultm[result.Digest] = attributes
+
+			}
+			oattributes := make(map[string]map[string]json.RawMessage)
+			for schema, attr := range attributeMap {
+
+				pair, err := json.Marshal(attr)
+				if err != nil {
+					log.Printf("Error while filtering: %v", err)
+				}
+				var jsonData map[string]interface{}
+				log.Println("unmarshalling annotations")
+				if err := json.Unmarshal([]byte(pair), &jsonData); err != nil {
+					log.Printf("Error while filtering: %v", err)
+				}
+				for key, value := range jsonData {
+					jsonValue, err := json.Marshal(value)
+					if err != nil {
+						log.Printf("Error while filtering: %v", err)
+					}
+					oa := make(map[string]json.RawMessage)
+					oa[key] = jsonValue
+					oattributes[schema] = oa
+
+				}
+
+			}
+			var keep []digest.Digest
+
+			// for each digest
+			for digest, fattribute := range filter {
+				// for each queried attribute
+				for oschema, oattribute := range oattributes {
+					for okey, oattr := range oattribute {
+
+						// if the queried attribute does not exist with the digest return false
+						var found bool
+						if fattribute[oschema] != nil {
+							for _, fattrib := range fattribute {
+								if fattrib[okey] != nil {
+									for key, fvals := range fattrib {
+										for _, fval := range fvals {
+											if string(fval) == string(oattr) {
+												found = true
+												log.Printf("key %s found!", key)
+											} else {
+												found = false
+												break
+											}
+										}
+										if !found {
+											break
+										}
+									}
+									if !found {
+										break
+									}
+								}
+							}
+							if !found {
+								break
+							}
+						}
+						if found {
+							keep = append(keep, digest)
+						}
+					}
+				}
+			}
+
+			for _, d := range keep {
+				resultm[d] = allresultm[d]
+			}
 		}
-	}
+		// Query links
+		if l := values.Get("links"); l != "" {
 
-	// Query links
-	if l := values.Get("links"); l != "" {
+			log.Printf("links parameter hit: %v", l)
 
-		log.Printf("links parameter hit: %v", l)
+			links := strings.Split(l, ",")
 
-		links := strings.Split(l, ",")
+			ln, _ := json.Marshal(links)
+			log.Printf("resolving %v", string(ln))
 
-		ln, _ := json.Marshal(links)
-		log.Printf("resolving %v", string(ln))
+			log.Println("query was split")
+			resolvedLinks, err := uor.LinkQuery(links, ah.database)
+			if err != nil {
+				log.Printf("error reading db: %v", err)
+			}
 
-		log.Println("query was split")
-		resolvedLinks, err := uor.LinkQuery(links, ah.database)
+			for linker, target := range resolvedLinks {
+				coreLink := make(map[string]string) // core-link/registryHint/NamespaceHints
+
+				t, err := json.Marshal(target)
+				if err != nil {
+					log.Printf("error reading db: %v", err)
+				}
+
+				log.Printf("link target: %v", string(t))
+
+				coreLink["core-link"] = string(t)
+
+				resultm[linker] = coreLink
+			}
+		}
+
+		// Resolve digests
+		// Query by digest
+		d := values.Get("digests")
+		var digests []string
+		if d != "" {
+			digests = strings.Split(d, ",")
+		}
+		for k := range resultm {
+			digests = append(digests, k.String())
+			log.Printf("Adding digest: %v to digest query", k.String())
+
+		}
+
+		resolvedDigests, err := uor.DigestQuery(digests, ah.database)
+		log.Printf("Resolved digest map: %v", resolvedDigests)
+
 		if err != nil {
 			log.Printf("error reading db: %v", err)
 		}
 
-		for linker, target := range resolvedLinks {
-			coreLink := make(map[string]string) // core-link/registryHint/NamespaceHints
+		for _, target := range resolvedDigests {
+			localNamespaces := make(map[string]string) // core-link/registryHint/NamespaceHints
 
 			t, err := json.Marshal(target)
 			if err != nil {
 				log.Printf("error reading db: %v", err)
 			}
 
-			log.Printf("link target: %v", string(t))
+			localNamespaces["LocalNamespaces"] = string(t)
 
-			coreLink["core-link"] = string(t)
-
-			resultm[linker] = coreLink
-		}
-	}
-
-	// Resolve digests
-	// Query by digest
-	d := values.Get("digests")
-	var digests []string
-	if d != "" {
-		digests = strings.Split(d, ",")
-	}
-	for k := range resultm {
-		digests = append(digests, k.String())
-		log.Printf("Adding digest: %v to digest query", k.String())
-
-	}
-
-	resolvedDigests, err := uor.DigestQuery(digests, ah.database)
-	log.Printf("Resolved digest map: %v", resolvedDigests)
-
-	if err != nil {
-		log.Printf("error reading db: %v", err)
-	}
-
-	for _, target := range resolvedDigests {
-		localNamespaces := make(map[string]string) // core-link/registryHint/NamespaceHints
-
-		t, err := json.Marshal(target)
-		if err != nil {
-			log.Printf("error reading db: %v", err)
+			resultm[target.Target] = localNamespaces
 		}
 
-		localNamespaces["LocalNamespaces"] = string(t)
+		// Write the index manifest with the resolved descriptors
+		for digest, result := range resultm {
 
-		resultm[target.Target] = localNamespaces
-	}
+			ann, _ := json.Marshal(result)
+			uorAttrib := make(map[string]string)
+			uorAttrib["uor.attributes"] = string(ann)
+			desc := v1.Descriptor{
+				MediaType:   v1.MediaTypeImageIndex,
+				Size:        0,
+				Digest:      digest,
+				Annotations: uorAttrib,
+				Platform:    nil,
+				URLs:        nil,
+			}
 
-	// Write the index manifest with the resolved descriptors
-	for digest, result := range resultm {
+			manifest.Manifests = append(manifest.Manifests, desc)
 
-		ann, _ := json.Marshal(result)
-		uorAttrib := make(map[string]string)
-		uorAttrib["uor.attributes"] = string(ann)
-		desc := v1.Descriptor{
-			MediaType:   v1.MediaTypeImageIndex,
-			Size:        0,
-			Digest:      digest,
-			Annotations: uorAttrib,
-			Platform:    nil,
-			URLs:        nil,
 		}
 
-		manifest.Manifests = append(manifest.Manifests, desc)
+		manifest.SchemaVersion = 2
 
-	}
+		// Return the response to the caller
 
-	manifest.SchemaVersion = 2
+		w.Header().Set("Content-Type", "application/json")
 
-	// Return the response to the caller
-
-	w.Header().Set("Content-Type", "application/json")
-
-	enc := json.NewEncoder(w)
-	if err := enc.Encode(attributesAPIResponse{manifest}); err != nil {
-		ah.Errors = append(ah.Errors, errcode.ErrorCodeUnknown.WithDetail(err))
-		return
+		enc := json.NewEncoder(w)
+		if err := enc.Encode(attributesAPIResponse{manifest}); err != nil {
+			ah.Errors = append(ah.Errors, errcode.ErrorCodeUnknown.WithDetail(err))
+			return
+		}
 	}
 }
